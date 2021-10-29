2bab4d2e4eb353bd77955c56ca4d962ba65647a7
josiasveras <josias_veras@hotmail.com>
2021-10-29 09:51:48 -0300

    Project creation

---
+++ src/main/java/com/verasmobile/conceptualmodeling/ConceptualmodelingApplication.java
@@ -0,0 +1,13 @@
+package com.verasmobile.conceptualmodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptualmodelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptualmodelingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verasmobile/conceptualmodeling/ConceptualmodelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verasmobile.conceptualmodeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptualmodelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
