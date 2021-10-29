42b073e16e4295c57000221fc603568f187f2fc2
josiasveras <josias_veras@hotmail.com>
2021-10-29 11:00:50 -0300

    Domain class Category test

--- src/main/java/com/verasmobile/conceptualmodeling/resources/CategoryResource.java
+++ src/main/java/com/verasmobile/conceptualmodeling/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.verasmobile.conceptualmodeling.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.verasmobile.conceptualmodeling.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String RestTest() {
-		return "REST is working!";
+	public List<Category> RestTest() {
+		
+		Category cat1 = new Category(1, "Computers");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
