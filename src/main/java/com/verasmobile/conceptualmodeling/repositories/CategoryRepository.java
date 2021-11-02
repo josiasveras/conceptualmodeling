346f74c37eefb2e88f4af404c24d6966a1524cb0
josiasveras <josias_veras@hotmail.com>
2021-11-02 15:20:26 -0300

    Add repository and service to Category

---
+++ src/main/java/com/verasmobile/conceptualmodeling/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.verasmobile.conceptualmodeling.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.verasmobile.conceptualmodeling.domain.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
