551f62dba3085ae04d3e773789316bf82f3f9a2a
josiasveras <josias_veras@hotmail.com>
2021-11-03 11:27:17 -0300

    Object Category instantiation

--- src/main/java/com/verasmobile/conceptualmodeling/ConceptualmodelingApplication.java
+++ src/main/java/com/verasmobile/conceptualmodeling/ConceptualmodelingApplication.java
@@ -1,13 +1,31 @@
 package com.verasmobile.conceptualmodeling;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.verasmobile.conceptualmodeling.domain.Category;
+import com.verasmobile.conceptualmodeling.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class ConceptualmodelingApplication {
+public class ConceptualmodelingApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualmodelingApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computers");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));	
+	}
 }
